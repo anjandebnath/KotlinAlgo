4306a7c251fcb5d000629f04207a0bf2b7ff1020
Anjan <anjan@w3engineers.com>
2021-11-04 18:44:45 +0600

    documentation update

--- src/main/kotlin/RemoveDuplicateFromLinkedList.java
+++ src/main/kotlin/RemoveDuplicateFromLinkedList.java
@@ -15,7 +15,8 @@ public class RemoveDuplicateFromLinkedList {
         }
     }
 
-    public static LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList){
+    public static LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList){ // formal parameter
+
         LinkedList currentNode = linkedList;
         while(currentNode != null) {
 
@@ -36,11 +37,14 @@ public class RemoveDuplicateFromLinkedList {
     // the head of linked list will be the first value
     // current linked list is used to convert the current array value
     // to a linked list and linked it to main linked list
-    public static LinkedList addMany(LinkedList ll, List<Integer> values){
+
+    //  Formal parameters are those parameters that are defined during function definition
+    public static LinkedList addMany(LinkedList ll, List<Integer> values){ // Formal parameters
         // copy object to another object
         /**
-         * original current object is released and available for garbage collection and
-         * now current holds the reference of ll object, which mean current and ll are referring the same object now, which is ll.
+         * original "current" object is released and available for garbage collection and
+         * now "current" holds the reference of ll object, which mean current and ll are referring the same object now, which is ll.
+         * any change on "current" object will be reflected on ll
          */
         LinkedList current = ll;
         while (current.next != null) {
@@ -82,7 +86,15 @@ public class RemoveDuplicateFromLinkedList {
          * to form linkedlist arraylist get converted to linkedList
          */
         LinkedList linkedList = addMany(head, new ArrayList<>(Arrays.asList(1,3,4,4,4,5,6,6)));
-        LinkedList removeDuplicate = removeDuplicatesFromLinkedList(linkedList);
+        /**
+         * In order to pass the reference, we pass the object of the class
+         * in the place of the actual parameter, and the formal parameter of a class object type
+         * has the same reference to each other that’s why with the help of the
+         * formal parameter object of class any changes will be reflected in both objects formal and actual objects.
+         */
+
+        // Actual parameters are those which are passed during the function call in other Function
+        LinkedList removeDuplicate = removeDuplicatesFromLinkedList(linkedList); // This is actual parameters
         List<Integer> nodes = getNodesInArray(removeDuplicate);
         for (int i=0; i<nodes.size(); i++){
             int node = nodes.get(i);
